daa1d90ee789ce40dd1907143b3614f812884055
flyingseal66 <2576209723@qq.com>
2019-01-07 20:40:21 +0000

    add annotations

---
+++ src/main/java/com/cbl/concurrency/annatations/NotRecommend.java
@@ -0,0 +1,16 @@
+package com.cbl.concurrency.annatations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * In this project, it's used to mark classes that are [not recommended] in threads
+ *
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+    String value() default "";
+}
